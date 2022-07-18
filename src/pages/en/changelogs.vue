84029426a607741a0bd6925be3f1e2765a234168
CI010 <cijhn@hotmail.com>
2022-07-18 16:23:37 +0800

    docs: Update 0.30.1 release note
    * docs: Update 0.30.1 release note
    
    * update changelog
    
    Co-authored-by: ci010 <ci010@users.noreply.github.com>

---
+++ src/pages/en/changelogs.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="">
+        <h1 class="text-5xl font-bold text-amber-400 leading-8 text-center">
+            Changelogs
+        </h1>
+        <router-view></router-view>
+    </div>
+</template>
+<script lang="ts" setup>
+
+useHead({
+    link: [
+        {
+            rel: "stylesheet",
+            crossorigin: "anonymous",
+            href: "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.6/dist/components/list.min.css",
+        },
+        {
+            rel: "stylesheet",
+            crossorigin: "anonymous",
+            href: "https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.6/dist/components/table.min.css",
+        }
+    ]
+})
+
+onMounted(() => {
+    const body = document.getElementsByClassName('markdown-body')
+    const tables = body.item(0)?.getElementsByTagName('table')
+    tables?.item(0)?.classList.add('ui', 'celled', 'inverted', 'table')
+})
+
+</script>
+
+<route lang="yaml">
+meta:
+  layout: changelog
+</route>
+
+
