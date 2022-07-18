52a9f413760451b5dc3549d015d6faa654a3b72d
CI010 <cijhn@hotmail.com>
2022-07-18 16:22:58 +0800

    update changelog

--- src/pages/en/changelogs/index.vue
+++ src/pages/en/changelogs/index.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="my-30">
-        <router-view></router-view>
+    <div class="gap-2 p-4 mt-10 flex flex-col gap-4">
+        <router-link v-for="ver of versions" :to="`/zh/changelogs/${ver.version}`" :key="ver.version" class="rounded border p-4 hover:(border-teal-400) text-gray-400 hover:(text-white) transition-all flex items-center cursor-pointer">
+            <div class="leading-10 text-3xl transition-all">
+                v{{ ver.version }}
+            </div>
+            <div class="flex-grow"></div>
+            <span class="text-gray-400 ml-2"> {{ new Date(ver.date).toLocaleDateString() }} </span>
+        </router-link>
     </div>
-    <Footer />
 </template>
 <script lang="ts" setup>
 
@@ -21,6 +26,11 @@ useHead({
     ]
 })
 
+const versions = Object.values(import.meta.globEager('./*.md')).map((r: any) => {
+    const { date, version } = r
+    return { date, version }
+}).reverse()
+
 onMounted(() => {
     const body = document.getElementsByClassName('markdown-body')
     const tables = body.item(0)?.getElementsByTagName('table')
@@ -29,3 +39,10 @@ onMounted(() => {
 })
 
 </script>
+
+<route lang="yaml">
+meta:
+  layout: changelog
+</route>
+
+
