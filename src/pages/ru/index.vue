9c2a1ef0a8f2cd8e70457e52bf72315c0b84ada4
CI010 <cijhn@hotmail.com>
2022-10-30 03:13:16 +0800

    fix ru page

---
+++ src/pages/ru/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <index />
+</template>
+<script lang="ts" setup>
+import { useLocaledPage } from '~/composables/localedPage';
+import index from '../index.vue'
+useLocaledPage('ru')
+</script>
