184f9d1a2268312f67c79f8b18e7b30619e0437a
CI010 <cijhn@hotmail.com>
2023-06-15 20:15:57 +0800

    Try to fix docs build

--- scripts/docs.ts
+++ scripts/docs.ts
@@ -2,12 +2,21 @@ import { readFile, writeFile } from "fs/promises";
 import { build } from "vitepress"
 
 async function transformDocs() {
-    const rootReadme = await readFile('docs/en/core/index.md', 'utf-8')
-    let modRootReadme = rootReadme.replaceAll('/packages/', '')
+    let rootReadme: string | undefined
+    try {
+        rootReadme = await readFile('docs/en/core/index.md', 'utf-8')
+        let modRootReadme = rootReadme.replaceAll('/packages/', '')
         .replace('(/CONTRIBUTE.md)', '(CONTRIBUTE.md)')
-    await writeFile('docs/en/core/index.md', modRootReadme, 'utf-8')
+        await writeFile('docs/en/core/index.md', modRootReadme, 'utf-8')
+    } catch {
+
+    }
+
     await build('docs')
-    await writeFile('docs/en/core/index.md', rootReadme, 'utf-8')
+
+    if (rootReadme) {
+        await writeFile('docs/en/core/index.md', rootReadme, 'utf-8')
+    }
 }
 
 transformDocs()
