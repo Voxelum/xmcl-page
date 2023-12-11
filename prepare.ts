7f2353948948b8c2cc0221b9e918433eca4a5e44
CI010 <cijhn@hotmail.com>
2023-12-11 19:28:39 +0800

    debug build

--- prepare.ts
+++ prepare.ts
@@ -1,5 +1,10 @@
 import { writeFileSync } from "fs";
+import { resolve } from "path";
 
+const path = resolve('.env')
 if (process.env.PROD && process.env.VITE_GITHUB_TOKEN) {
-    writeFileSync('./.env', `VITE_GITHUB_TOKEN=${process.env.VITE_GITHUB_TOKEN}`)
+    writeFileSync(path, `VITE_GITHUB_TOKEN=${process.env.VITE_GITHUB_TOKEN}`)
+    console.log(path, 'Write .env file successfully!')
+} else {
+    console.log(path, 'No need to write .env file.', !!process.env.PROD, !!process.env.VITE_GITHUB_TOKEN)
 }
