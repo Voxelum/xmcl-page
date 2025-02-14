9eda6a853daa1b2c5365cab3bd4f6476a957aae8
ci010 <cijhn@hotmail.com>
2025-02-14 17:49:42 +0800

    revert changes

--- .vitepress/theme/composables/runs.ts
+++ .vitepress/theme/composables/runs.ts
@@ -9,8 +9,15 @@ export type WorkflowRun = {
     // Add more properties as needed
 };
 
-export async function getRuns(): Promise<WorkflowRun[]> {
-    const runResponse = await fetch(`https://api.xmcl.app/prebuilds`, {
+export async function getRuns(token: string): Promise<WorkflowRun[]> {
+    const runResponse = await fetch(`https://api.github.com/repos/voxelum/x-minecraft-launcher/actions/workflows/1220495/runs`, {
+        headers: token ? {
+            Authorization: `token ${token}`,
+        } : {},
     });
-    return await runResponse.json() as WorkflowRun[];
+    return processData(await runResponse.json() as any)
+}
+
+const processData = (data: { workflow_runs: WorkflowRun[] }) => {
+    return data.workflow_runs.filter(r => r.status !== 'in_progress').sort((a, b) => a.created_at > b.created_at ? -1 : 1);
 }
